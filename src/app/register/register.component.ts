71aac4aa6f81a2c464c9ffd06f904c48028c4991
Sorin B <niros98@gmail.com>
2018-09-27 13:32:19 +0300

    CSS to SCSS

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,7 +6,7 @@ import { UserModel } from 'src/app/shared/user.model';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
--- src/app/service/service-details/service-details.component.ts
+++ src/app/service/service-details/service-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-service-details',
   templateUrl: './service-details.component.html',
-  styleUrls: ['./service-details.component.css']
+  styleUrls: ['./service-details.component.scss']
 })
 export class ServiceDetailsComponent implements OnInit {
 
