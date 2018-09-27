904a646430a1d8784d162bb37744217e31966ada
Sorin B <niros98@gmail.com>
2018-09-27 13:47:46 +0300

    Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/services/user.service';
 
+//import * as $ from 'jquery';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,6 +13,11 @@ export class HeaderComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    /*
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
+    */
   }
 
   isAuth() {
