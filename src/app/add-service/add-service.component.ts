e24d5a3f4a0c7f4b8345f1b3b7f197a258a4d2be
Sorin B <niros98@gmail.com>
2018-09-27 13:32:19 +0300

    CSS to SCSS

--- src/app/add-service/add-service.component.ts
+++ src/app/add-service/add-service.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-service',
   templateUrl: './add-service.component.html',
-  styleUrls: ['./add-service.component.css']
+  styleUrls: ['./add-service.component.scss']
 })
 export class AddServiceComponent implements OnInit {
 
