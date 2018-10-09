4bacf5c2b6bf2a84c7042e9065b47c9bcc2667ab
Sorin B <niros98@gmail.com>
2018-10-10 01:06:48 +0300

    Service

--- src/app/dashboard/services/services.component.ts
+++ src/app/dashboard/services/services.component.ts
@@ -8,11 +8,39 @@ import { ServiceModel } from 'src/app/shared/service.model';
 })
 export class ServicesComponent implements OnInit {
 
-  @Input() services: ServiceModel;
+  @Input() services: ServiceModel[];
 
   constructor() { }
 
   ngOnInit() {
+    this.services = [
+      new ServiceModel(
+        true,
+        "Titlululululululu",
+        "Descrierererererere",
+        3,
+        "Categorie",
+        [],
+        "bSUKBJOsf5NnZvuv5v6maicWeON2",
+        "cine stie",
+        true,
+        "In Progress",
+        69.99
+      ),
+      new ServiceModel(
+        true,
+        "Titlululululululu2",
+        "Descrierererererere2",
+        5,
+        "Alta Categorie",
+        [],
+        "bSUKBJOsf5NnZvuv5v6maicWeON2",
+        "cine stie",
+        false,
+        "Complete",
+        99.99
+      )
+    ];
   }
 
 }
